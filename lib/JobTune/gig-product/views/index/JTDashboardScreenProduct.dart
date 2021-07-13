2b52b0fd055d46852967691fbe44da116f2b50ac
shahirahrahim <n.shahirah@theiac.com>
2021-07-13 14:38:01 +0800

    gig product

---
+++ lib/JobTune/gig-product/views/index/JTDashboardScreenProduct.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:prokit_flutter/defaultTheme/screen/DTDashboardWidget.dart';
+import 'package:prokit_flutter/defaultTheme/screen/DTDrawerWidget.dart';
+import 'package:prokit_flutter/main.dart';
+import 'package:prokit_flutter/main/utils/AppWidget.dart';
+
+
+class JTDashboardScreenProduct extends StatefulWidget {
+  @override
+  _JTDashboardScreenProductState createState() => _JTDashboardScreenProductState();
+}
+
+class _JTDashboardScreenProductState extends State<JTDashboardScreenProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
+
+
