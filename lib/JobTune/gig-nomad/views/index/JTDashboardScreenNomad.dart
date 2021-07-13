97ea7b66ff95c1d4db7e938995160ce5e577f7bc
shahirahrahim <n.shahirah@theiac.com>
2021-07-13 14:37:52 +0800

    gig nomad

---
+++ lib/JobTune/gig-nomad/views/index/JTDashboardScreenNomad.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:prokit_flutter/defaultTheme/screen/DTDashboardWidget.dart';
+import 'package:prokit_flutter/defaultTheme/screen/DTDrawerWidget.dart';
+import 'package:prokit_flutter/main.dart';
+import 'package:prokit_flutter/main/utils/AppWidget.dart';
+
+
+class JTDashboardScreenNomad extends StatefulWidget {
+  @override
+  _JTDashboardScreenNomadState createState() => _JTDashboardScreenNomadState();
+}
+
+class _JTDashboardScreenNomadState extends State<JTDashboardScreenNomad> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
